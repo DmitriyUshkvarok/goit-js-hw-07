a7b0797782656e5a95f15f89dcdbca5ac6af6415
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2022-11-12 23:13:13 +0200

    update home work

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,7 @@
 import { galleryItems } from "./gallery-items.js";
 // создаём переменную галерее
 const gallery = document.querySelector(".gallery");
+
 // создаём функцию в которой возвращаем копию массива обьектов с добавленой разметкой
 const newGallery = (items) => {
   return (
@@ -22,30 +23,34 @@ const newGallery = (items) => {
       .join("")
   );
 };
-// console.log(gallery);
 
 // добавляем  созданную разметку в галерею
 gallery.innerHTML = newGallery(galleryItems);
 // создаём событие по клику для галереи
-gallery.addEventListener("click", (event) => {
-  // сбрасываем дефолтное поведение для браузера
+gallery.addEventListener("click", clickOnImg);
+
+// сбрасываем дефолтное поведение для браузера
+function clickOnImg(event) {
+  console.log(event);
   event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
   // создаём переменную для для открытия оригинального изображения
   const selectedImage = event.target.getAttribute("data-source");
   const instance = basicLightbox.create(`
     <img src="${selectedImage}" width="1200" height="800">
 `);
-  // пишем метод библиотеки для показа главного изображения
+
   instance.show();
-  if (event.target === "IMG") {
-    return instance.close();
-  }
-  console.log(event.target);
+
   // создаём событие для командных кнопок и делаем в нём проверку  на строгое равенство с кнопкой esc
   gallery.addEventListener("keydown", (closed) => {
     if (closed.key === "Escape") {
       instance.close();
     }
   });
-  console.log(basicLightbox);
-});
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+const gallery = document.querySelector(".gallery");
+
+const newGallery = (items) => {
+  return galleryItems
+    .map(
+      (item) =>
+        `<li>
+              <a class="gallery__item" href=${item.original}>
+              <img class="gallery__image" src=${item.preview} alt="${item.description}" />
+              </a>
+          </li>`
+    )
+    .join("");
+};
+gallery.innerHTML = newGallery(galleryItems);
 
 console.log(galleryItems);
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+});
